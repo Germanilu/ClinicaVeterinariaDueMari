85c8175d95e1efd1ce6c8470fbfefbb8a4a5d3c1
Germanilu <lucianogermani93@gmail.com>
2022-08-23 13:26:12 +0200

    Feat: Create basic login style

--- src/Containers/User/Login/Login.jsx
+++ src/Containers/User/Login/Login.jsx
@@ -3,7 +3,30 @@ import './Login.scss';
  
 const Login = () => {
      return (
-         <div className='loginDesign'>HOLA SOY LOGIN</div>
+         <div className='loginDesign'>
+             <div className="containerLogin">
+                 <div className="containerImg">Here IMG</div>
+                 <div className="containerDataLogin">
+                     <h1>Clinica Veterinaria DueMari</h1>
+                     <h3>Login</h3>
+                     {/* Inputs */}
+                     <div className="inputsContainer">
+                         <div className="inputEmail">
+                             <p className='inputPar'>Email</p>
+                             <input type="email" />
+                         </div>
+                         <div className="inputPassword">
+                             <p className='inputPar'>Password</p>
+                             <input type="password" />
+                         </div>
+                         <div className="buttonLogin">Login</div>
+                         <div className="containerRegister">
+                            <p>Non sei ancora registrato?  ---Arrow</p>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
      )
 }
 export default Login;
