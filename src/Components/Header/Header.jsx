1b9847f194d47c979634f614171fd9aaaf36e469
Germanilu <lucianogermani93@gmail.com>
2022-08-23 11:35:26 +0200

    Feat: SetUp Header principal Components

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 import './Header.scss'
 const Header = () => {
      return (
-         <div className='headerDesign'> SOY HEADER</div>
+         <div className='headerDesign'>
+            <div className="logo">Logo Clinica</div>
+            <div className="containerMenu">
+                <div className="headerButton">Consulto Online</div>
+                <div className="headerButton">Richiedi Appuntamento</div>
+                <div className="headerButton">Accedi</div>
+            </div>
+         </div>
      )
 }
 export default Header;
