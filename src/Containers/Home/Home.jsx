84ebd7e122e6062d28d5cbea8398e607f15f8a39
Germanilu <lucianogermani93@gmail.com>
2022-08-26 13:24:12 +0200

    Feat: Create first section Home

--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import './Home.scss'
 const Home = () => {
-     return (
-         <div className='homeDesign'>SOY HOME</div>
-     )
+    return (
+        <div className='homeDesign'>
+            <div className="firstSection">
+                <div className="containerInfo">
+                    <div className="containerText">
+                        <h1>Clinica Veterinaria Duemari</h1>
+                        <p>Hai un problema che riguarda la salute del tuo amico animale? <br />  Ti serve un consiglio o parere? <br /><br /> <strong>Contattaci</strong> accedendo al servizio di consulta online in fondo alla pagina o vienici a trove direttamente in clinica! <br /> </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 export default Home;
