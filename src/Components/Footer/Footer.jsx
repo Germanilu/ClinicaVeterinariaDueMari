954f7c10e284d824e10e4db04dd0c7517aa2eabb
Germanilu <lucianogermani93@gmail.com>
2022-08-28 10:02:16 +0200

    Feat: Footer Complete with links to social & logo

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,35 +1,46 @@
 import React from 'react';
 import './Footer.scss'
+import { useNavigate } from 'react-router-dom';
 
+//Svg
 import { ReactComponent as Fb } from '../../img/fb.svg'
-import { ReactComponent as Ig } from '../../img/ig.svg'
 import { ReactComponent as Yt } from '../../img/yt.svg'
-import { ReactComponent as Tw } from '../../img/tw.svg'
+import { ReactComponent as Palla } from '../../img/palla.svg'
 
 
 const Footer = () => {
+    //Const
+    const navigate = useNavigate()
+
+    //Function to navigate 
+    const move = (i) => {
+        navigate(i)
+    }
 
     return (
         <div className='footerDesign'>
-            <div className="footerLogo">Logo Clinica</div>
+            <div className="logo" onClick={() => move('/')}><img src={require('../../img/logoBlur.png')} alt="Logo Duemari" className='logo' /></div>
             <div className="socialContainer">
                 <ul class="wrapper">
-                    <li class="icon facebook">
-                        <span class="tooltip">Facebook</span>
-                        <span><i class="fab fa-facebook-f"><Fb/></i></span>
-                    </li>
-                    <li class="icon twitter">
-                        <span class="tooltip">Twitter</span>
-                        <span><i class="fab fa-twitter"><Tw/></i></span>
-                    </li>
-                    <li class="icon instagram">
-                        <span class="tooltip">Instagram</span>
-                        <span><i class="fab fa-instagram"><Ig/></i></span>
-                    </li>
-                    <li class="icon youtube">
-                        <span class="tooltip">Youtube</span>
-                        <span><i class="fab fa-youtube"><Yt/></i></span>
-                    </li>
+                    <a href="https://www.facebook.com/clinica.duemari/">
+                        <li class="icon facebook">
+
+                            <span class="tooltip">Facebook</span>
+                            <span><i class="fab fa-facebook-f"><Fb /></i></span>
+                        </li>
+                    </a>
+                    <a href="http://www.effettopallaonlus.it/">
+                        <li class="icon onlus">
+                            <span class="tooltip">Onlus</span>
+                            <span><i class="fab fa-onlus"><Palla /></i></span>
+                        </li>
+                    </a>
+                    <a href="https://www.youtube.com/channel/UCFOQ9UPNZvWb_fibjl19s3A">
+                        <li class="icon youtube">
+                            <span class="tooltip">Youtube</span>
+                            <span><i class="fab fa-youtube"><Yt /></i></span>
+                        </li>
+                    </a>
                 </ul>
             </div>
         </div>
