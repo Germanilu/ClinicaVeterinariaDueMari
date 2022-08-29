a44f7c02a66a3b05a8ab481326444870b0764708
Germanilu <lucianogermani93@gmail.com>
2022-08-29 20:53:34 +0200

    Feat: Basic profile card

--- src/Containers/User/Profile/Profile.jsx
+++ src/Containers/User/Profile/Profile.jsx
@@ -1,8 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Profile.scss'
+import { userData } from '../../User/userSlice';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
 const Profile = () => {
-     return (
-         <div className='profileDesign'></div>
-     )
+
+    const credentials = useSelector(userData);
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        console.log(credentials)
+    })
+
+
+    useEffect(() => {
+        // if (credentials.token === '') {
+        //     navigate('/login')
+        // }
+    })
+
+    return (
+        <div className='profileDesign'>
+
+            <div className="profileContainer">
+                <h1>Il mio Profilo</h1>
+                <div className="userInfo">
+                    <p><strong>Nome: </strong>{credentials.user_name}</p>
+                    <p><strong>Cognome: </strong>{credentials.user_surname}</p>
+                    <p><strong>Telefono: </strong> {credentials.user_mobile}</p>
+                    <p><strong>Indirizzo: </strong> {credentials.user_address} {credentials.user_city}</p>
+                    <p> <strong>Email: </strong>{credentials.user_email}</p>
+
+                </div>
+                <div className="containerButtonProfile">
+                    <div className="button buttonProfile">Modifica Profilo</div>
+                </div>
+            </div>
+
+            <div className="petContainer">
+                <h1>I Miei Animali</h1>
+            </div>
+        </div>
+    )
 }
 export default Profile;
