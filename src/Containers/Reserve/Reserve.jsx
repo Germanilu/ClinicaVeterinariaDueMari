6d6fdc04ed502aad31bc7b2bcde1699c4bd7a7c8
Germanilu <lucianogermani93@gmail.com>
2022-08-29 13:56:49 +0200

    Feat: Create Logic Reserve

--- src/Containers/Reserve/Reserve.jsx
+++ src/Containers/Reserve/Reserve.jsx
@@ -3,14 +3,46 @@ import './Reserve.scss'
 
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
-
+import { userData } from '../../Containers/User/userSlice';
+import { useSelector } from 'react-redux';
+import axios from 'axios';
 
 
 
 const Reserve = () => {
 
+    //Var
+    const credentials = useSelector(userData);
+    //Hooks
     const [date, onChange] = useState(new Date());
-    console.log(date)
+    const [hour,setHour] = useState() 
+  
+    const reserve = async() => {
+        try {
+            let config = {
+                headers: { Authorization: `Bearer ${credentials.token}` }
+            };
+
+            //.toDateString transformy date + time into date only, and avoid issue with date select to post request with previous da
+            let body = {
+                date: date.toDateString(),
+                hour:hour
+            };
+
+            console.log(body)
+
+            const attempt = await axios.post("https://bbdd-cv2.herokuapp.com/api/booking", body, config)
+            console.log(attempt)
+            
+        } catch (error) {
+            console.log(error)
+        }
+    }
+  
+    
+    const updateHour = (e) => {
+        setHour(e.target.value)
+    }
 
     return (
         <div className='reserveDesign'>
@@ -22,14 +54,14 @@ const Reserve = () => {
                 </div>
                 <div className="containerHour">
                     <h1>Seleziona un orario</h1>
-                    <select name="" id="">
-                        <option value="">12:30</option>
-                        <option value="">13:00</option>
-                        <option value="">13:30</option>
+                    <select name="hour" onChange={updateHour} value="dd">
+                        <option value="12:30">12:30</option>
+                        <option value="13:00">13:00</option>
+                        <option value="13:30">13:30</option>
                     </select>
                 </div>
             </div>
-            <div className="button">Richiedi Appuntamento</div>
+            <div className="button" onClick={() => reserve()} >Richiedi Appuntamento</div>
 
 
 
