6b477d2ff8deb8356df2785108f314ed1835b20e
Germanilu <lucianogermani93@gmail.com>
2022-08-31 11:45:49 +0200

    Feat: MyConsult Logic Complete

--- src/Containers/User/Profile/MyConsult/MyConsult.jsx
+++ src/Containers/User/Profile/MyConsult/MyConsult.jsx
@@ -2,17 +2,22 @@ import React, { useEffect, useState } from 'react';
 import './MyConsult.scss'
 import { userData } from '../../userSlice';
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 const MyConsult = () => {
 
     //Var
     const credentials = useSelector(userData);
+    const navigate = useNavigate()
+
+    //Hooks
     const [allConsult, setAllConsult] = useState([])
+    const [show, setShow] = useState()
 
     useEffect(() => {
         consult()
-    },[] )
+    }, [])
 
 
     useEffect(() => {
@@ -21,7 +26,7 @@ const MyConsult = () => {
         // }
     })
 
-    const consult = async() => {
+    const consult = async () => {
         try {
             let config = {
                 headers: { Authorization: `Bearer ${credentials.token}` }
@@ -36,17 +41,36 @@ const MyConsult = () => {
         }
     }
 
-     return (
-         <div className='myConsultDesign'>
+    //Receive idConsult as parameter and change the state of the hook 
+    const isClick = (id) => {
+        show === "" ? setShow(id) : setShow("")
+    }
+
+    return (
+        <div className='myConsultDesign'>
             <div className="myConsultContainer">
-            {allConsult.map(element => (
-                        <div className="myConsultBox" key={element.id}>
-                            <div className="myConsultDate">{element.date}</div>
-                        </div>
-                        ))}
-                
+                {allConsult.length === 0 && (
+                    <p>
+                        Al momento non hai ancora richiesto nessun consulto Online, Vuoi richiedere un consulto? <span onClick={() => navigate('/consult')}>Clicca qui!</span>
+                    </p>
+                )}
+                {allConsult.length > 0 && (
+                    allConsult.map((element) => {
+                        return (
+                            <div className="myConsultBox" key={element._id} onClick={() => isClick(element._id)} > <strong>Data Consulto Online:</strong>  {element.date}
+                                <div className={show === element._id ? "showContainerConsult" : "hideContainerConsult"} >
+                                    <div className="containerConsultText"> <strong>Messaggio:</strong>  {element.userMessage}</div>
+                                    <div className="containerConsultText"> <strong>Risposta Veterinario:</strong>  {element.vetMessage}</div>
+                                </div>
+
+                            </div>
+                        )
+                    }
+                    )
+                )
+                }
             </div>
-         </div>
-     )
+        </div>
+    )
 }
 export default MyConsult;
