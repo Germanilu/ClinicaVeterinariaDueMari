adb3da226c5acd964451c1a36639d7af8c48a538
Germanilu <lucianogermani93@gmail.com>
2022-09-01 09:58:54 +0200

    Feat: VetConsult Logic complete

--- src/Containers/Consult/VetConsult/VetConsult.jsx
+++ src/Containers/Consult/VetConsult/VetConsult.jsx
@@ -17,6 +17,9 @@ const VetConsult = () => {
     const [unreplyConsult, setUnreplyConsult] = useState([])
     const [replyConsult, setReplyConsult] = useState([])
     const [show, setShow] = useState()
+    const [replyBox,setReplyBox] = useState()
+    const [reply, setReply] = useState("")
+    const [msg,setMsg] = useState()
    
 
     useEffect(() => {
@@ -29,9 +32,11 @@ const VetConsult = () => {
         // if (credentials.token === '') {
         //     navigate('/login')
         // }
+
+        
     })
 
-
+    //Verify all the existing unreply consult on DB
     const verifyUnreplyConsult = async () => {
         try {
             let config = {
@@ -46,6 +51,7 @@ const VetConsult = () => {
         }
     }
 
+    //Verify all the consult already reply by Vet
     const verifyReplyConsult = async () => {
         try {
             let config = {
@@ -60,10 +66,44 @@ const VetConsult = () => {
         }
     }
 
+    //Send Reply to Consult
+    const sendConsultReply = async(id) => {
+        try {
+            console.log(id)
+            console.log(reply)
+
+            const body = {
+                vetMessage: reply
+            }
+
+            let config = {
+                headers: { Authorization: `Bearer ${credentials.token}` }
+            };
+
+            const attempt = await axios.put(`https://bbdd-cv2.herokuapp.com/api/consult/${id}`,body, config)
+            setMsg(attempt.data.message)
+        } catch (error) {
+            setMsg(error.response.data.message)
+            setTimeout(() => {
+                setMsg("")
+            }, 3000);
+        }
+    }
+
 
     //Receive idConsult as parameter and change the state of the hook 
     const isClick = (id) => {
         show === "" ? setShow(id) : setShow("")
+    }
+
+    const isReply = (id) => {
+        replyBox === "" ? setReplyBox(id): setReplyBox("")
+    }
+
+
+      //Function to update the reply of the consult
+      const updateMessage = (e) => {
+        setReply(e.currentTarget.value)
     }
 
   
@@ -77,13 +117,19 @@ const VetConsult = () => {
                         <div className="consultBox" key={element._id} >
                             <div className="consultBoxHeader">
                                 <p><strong>Data Consulto Online: </strong>  {element.date}</p>
-                                <div className="button buttonConsult" onClick={() => isClick(element._id)}>Dettaglio</div>
-                                <div className="button buttonConsult" >Rispondi</div>
+                                <div className="button " onClick={() => isClick(element._id)}>Dettaglio</div>
+                                
                             </div>
 
 
                             <div className={show === element._id ? "showConsultBox" : "hideConsultBox"}>
                                 <div className="containerConsultText"><strong>Messaggio:</strong> {element.userMessage}</div>
+                                <div className="button" onClick={() => isReply(element._id)}>Rispondi</div>
+                                <div className={replyBox === element._id ? "showReplyBox": "hideReplyBox" }>
+                                    <textarea name="vetMessage" className='replyTextArea' onChange={updateMessage} ></textarea>
+                                    {msg}
+                                    <div className="button" onClick={() => sendConsultReply(element._id)}>Invia Risposta</div>
+                                </div>
 
                             </div>
                         </div>
