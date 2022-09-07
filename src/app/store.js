5c09da7a07a4c1e3e514927009481e3b62d1ced6
Germanilu <lucianogermani93@gmail.com>
2022-09-07 21:50:23 +0200

    Feat: Implement redux-Persist to avoid logout on refresh

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,23 @@
 import {configureStore} from '@reduxjs/toolkit';
 import userSlice from '../Containers/User/userSlice';
+//Redux persist to store userSlice on windowReload
+import storage from "redux-persist/lib/storage";
+import {persistReducer} from "redux-persist";
+import {combineReducers} from "redux";
+
+const persistConfig = {
+    key: "root",
+    version: 1,
+    storage,
+}
+
+const reducer = combineReducers({
+    user: userSlice,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducer);
 
 export default configureStore({
-    reducer: {
-        user: userSlice
-    }
+    reducer: persistedReducer,
 });
+
--- src/index.js
+++ src/index.js
@@ -7,12 +7,18 @@ import reportWebVitals from './reportWebVitals';
 //Redux
 import { Provider } from 'react-redux';
 import store from './app/store'
+import {persistStore} from "redux-persist";
+import {PersistGate} from "redux-persist/integration/react"
+
+let persistor = persistStore(store)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
     <Provider store={store}>
+      <PersistGate persistor={persistor}>
       <App />
+      </PersistGate>
     </Provider>
   
 );
